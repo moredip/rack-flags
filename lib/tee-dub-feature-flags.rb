5428befbabefa0cc1e5afd85305bda8dc6233f08
Pete Hodgson <git@thepete.net>
2013-05-07 18:07:08 -0700

    moving stuff around

--- lib/tee-dub-feature-flags.rb
+++ lib/tee-dub-feature-flags.rb
@@ -2,10 +2,10 @@ require 'tee-dub-feature-flags/version'
 require 'tee-dub-feature-flags/defaults'
 require 'tee-dub-feature-flags/flag_overrides'
 require 'tee-dub-feature-flags/derived_flags'
-require 'tee-dub-feature-flags/cookie_codec'
+require 'tee-dub/feature-flags/cookie_codec'
 
-require 'tee-dub-feature-flags/config'
-require 'tee-dub-feature-flags/reader'
+require 'tee-dub/feature-flags/config'
+require 'tee-dub/feature-flags/reader'
 
 require 'tee-dub-feature-flags/rack_middleware'
 require 'tee-dub-feature-flags/admin_app'
