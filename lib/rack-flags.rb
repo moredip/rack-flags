56ad5a3b2fb5f44e94ceaff2851cbec7d1a25b48
Ryan Oglesby <ryan.oglesby08@gmail.com>
2013-07-13 23:33:45 -0500

    Spike on switching the admin app to a Sinatra app for easier inclusion into Rails

--- lib/rack-flags.rb
+++ lib/rack-flags.rb
@@ -8,8 +8,4 @@ module RackFlags
   def self.for_env(env)
     env[RackMiddleware::ENV_KEY] || Reader.blank_reader
   end
-
-  def self.path_for_resource(subpath)
-    File.expand_path( File.join( "../../resources", subpath ), __FILE__ )
-  end
 end
--- lib/rack-flags/admin_app.rb
+++ lib/rack-flags/admin_app.rb
@@ -1,4 +1,4 @@
-require 'erb'
+require 'sinatra/base'
 
 module RackFlags
   class FullFlagPresenter
@@ -35,69 +35,33 @@ module RackFlags
 
   end
 
-  class AdminApp
-    def call(env)
-      request = Rack::Request.new(env)
+  class AdminApp < Sinatra::Base
+    set :public_folder, File.dirname(__FILE__) + '/../../resources/admin_app'
+    set :views,         File.dirname(__FILE__) + '/../../resources/admin_app'
 
-      if request.path_info.chomp("/").empty?
-        handle_root(request)
-      else
-        handle_non_root(request)
+    get '/' do
+      reader = RackFlags.for_env(request.env)
+      flag_presenters = reader.full_flags.map{ |flag| FullFlagPresenter.new(flag) }
+
+      erb :index, locals: {css_href: "#{request.path}/style.css", flags: flag_presenters}
+    end
+
+    post '/' do
+      overrides = request.POST.inject({}) do |overrides, (flag_name, form_param_flag_state)|
+        overrides[flag_name.downcase.to_sym] = flag_value_for(form_param_flag_state)
+        overrides
       end
+
+      cookie = CookieCodec.new.generate_cookie_from(overrides)
+
+      response = Rack::Response.new
+      response.redirect(request.script_name, 303)
+      response.set_cookie(CookieCodec::COOKIE_KEY, cookie)
+
+      response.finish
     end
 
     private
-
-      def handle_root(request)
-        case 
-        when request.get? then handle_root_get(request)
-        when request.post? then handle_root_post(request)
-        else
-          not_allowed
-        end
-      end
-
-      def handle_root_get(request)
-        reader = RackFlags.for_env(request.env)
-
-        template = ERB.new(resource('index.html.erb'))
-
-
-        flag_presenters = reader.full_flags.map{ |flag| FullFlagPresenter.new(flag) }
-        view_model = OpenStruct.new( 
-          :css_href => "#{request.path}/style.css",
-          :flags => flag_presenters
-        )
-
-        [
-          200, 
-          {'Content-Type'=>'text/html'}, 
-          [template.result( view_model.instance_eval{ binding } )]
-        ]
-      end
-
-      def handle_root_post(request)
-        overrides = request.POST.inject({}) do |overrides, (flag_name, form_param_flag_state)|
-          overrides[flag_name.downcase.to_sym] = flag_value_for(form_param_flag_state)
-          overrides
-        end
-
-        cookie = CookieCodec.new.generate_cookie_from(overrides)
-
-        response = Rack::Response.new
-        response.redirect(request.script_name, 303)
-        response.set_cookie(CookieCodec::COOKIE_KEY, cookie)
-
-        response.finish
-      end
-
-      def handle_non_root(request)
-        Rack::File.new( RackFlags.path_for_resource('admin_app') ).call(request.env)
-      end
-
-      def not_allowed
-        [405, {}, '405 - METHOD NOT ALLOWED']
-      end
 
       def flag_value_for(form_param_flag_state)
         flag_states = {
@@ -106,14 +70,6 @@ module RackFlags
             default: nil
         }
         flag_states[form_param_flag_state.to_sym]
-      end
-
-      def resource_root
-        RackFlags.path_for_resource('admin_app')
-      end
-
-      def resource(filename)
-        File.read(RackFlags.path_for_resource(File.join('admin_app',filename)))
       end
 
   end
