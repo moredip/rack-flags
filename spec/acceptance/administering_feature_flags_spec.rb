3b1445942eb8534742ba9b8a6aed52500b0de68d
Pete Hodgson <git@thepete.net>
2013-08-17 20:43:29 -0700

    introduce AdminPage Page Object

--- spec/acceptance/administering_feature_flags_spec.rb
+++ spec/acceptance/administering_feature_flags_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'spec_helper'
+require_relative 'support/page_objects/admin_page'
 
 describe 'displaying flags in admin app' do
-  include Capybara::DSL
 
   let( :feature_flag_config ) do
     {
@@ -29,65 +29,41 @@ describe 'displaying flags in admin app' do
     end
   end
 
-  let(:on_flag_section){ page.find('section[data-flag-name="on_by_default"]') }
-  let(:off_flag_section){ page.find('section[data-flag-name="off_by_default"]') }
-  let(:update_button){ page.find('input[type="submit"]') }
-
   it 'successfully GETs the admin page' do
-    visit '/'
-    status_code.should == 200
+    AdminPage.visiting do |page|
+      page.verify_status_code_is 200
+    end
   end
 
   it 'renders the feature flag name, default and description' do
-    visit '/'
+    AdminPage.visiting do |page|
+      on_flag_section = page.section_for_flag_named('on_by_default')
+      on_flag_section.should_not be_nil
+      on_flag_section.find('h3').text.should == 'on_by_default'
+      on_flag_section.find('p').text.should == 'this flag on by default'
+      on_flag_section.find('label.default').text.should include('Default (On)')
 
-    on_flag_section.should_not be_nil
-    on_flag_section.find('h3').text.should == 'on_by_default'
-    on_flag_section.find('p').text.should == 'this flag on by default'
-    on_flag_section.find('label.default').text.should include('Default (On)')
-
-    off_flag_section.should_not be_nil
-    off_flag_section.find('h3').text.should == 'off_by_default'
-    off_flag_section.find('p').text.should == 'this flag off by default'
-    off_flag_section.find('label.default').text.should include('Default (Off)')
+      off_flag_section = page.section_for_flag_named('off_by_default')
+      off_flag_section.should_not be_nil
+      off_flag_section.find('h3').text.should == 'off_by_default'
+      off_flag_section.find('p').text.should == 'this flag off by default'
+      off_flag_section.find('label.default').text.should include('Default (Off)')
+    end
   end
 
   it 'selects the default option if there are no cookies present' do
-    visit '/'
-
-    verify_flag_section( on_flag_section, :default )
-    verify_flag_section( off_flag_section, :default )
+    AdminPage.visiting do |page|
+      page.verify_flag_section( 'on_by_default', :default )
+      page.verify_flag_section( 'off_by_default', :default )
+    end
   end
 
   it 'allows switching off a flag defaulted to on' do
-    visit '/'
-    
-    on_flag_section.choose( 'Off' )
-    update_button.click
+    AdminPage.visiting do |page|
+      page.turn_off_flag_name('on_by_default')
 
-    verify_flag_section( on_flag_section, :off )
-    verify_flag_section( off_flag_section, :default )
-  end
-
-  def verify_flag_section( section, expected_state )
-    case expected_state.to_sym
-    when :default
-      section.find('label.default input').should be_checked
-
-      section.find('label.on input').should_not be_checked
-      section.find('label.off input').should_not be_checked
-    when :on
-      section.find('label.on input').should be_checked
-
-      section.find('label.default input').should_not be_checked
-      section.find('label.off input').should_not be_checked
-    when :off
-      section.find('label.off input').should be_checked
-
-      section.find('label.default input').should_not be_checked
-      section.find('label.on input').should_not be_checked
-    else 
-      raise "unrecognized state '#{expected_state}'"
+      page.verify_flag_section( 'on_by_default', :off )
+      page.verify_flag_section( 'off_by_default', :default )
     end
   end
 end
---
+++ spec/acceptance/support/page_objects/admin_page.rb
@@ -0,0 +1,61 @@
+class AdminPage
+  include RSpec::Matchers
+  include Capybara::DSL
+
+  def self.visiting( path = "/" )
+    page = self.new(path)
+    page.visit_page
+    yield page
+  end
+
+  def initialize( path )
+    @path = path
+  end
+
+  def visit_page
+    visit @path
+  end
+
+  def turn_off_flag_name(flag_name)
+    section_for_flag_named(flag_name).choose('Off')
+    update_button.click
+  end
+
+
+  def update_button 
+    page.find('input[type="submit"]')
+  end
+
+  def section_for_flag_named(flag_name)
+    page.find(%Q|section[data-flag-name="#{flag_name}"]|)
+  end
+
+  def verify_flag_section( flag_name, expected_state )
+    section = section_for_flag_named( flag_name )
+
+    case expected_state.to_sym
+    when :default
+      section.find('label.default input').should be_checked
+
+      section.find('label.on input').should_not be_checked
+      section.find('label.off input').should_not be_checked
+    when :on
+      section.find('label.on input').should be_checked
+
+      section.find('label.default input').should_not be_checked
+      section.find('label.off input').should_not be_checked
+    when :off
+      section.find('label.off input').should be_checked
+
+      section.find('label.default input').should_not be_checked
+      section.find('label.on input').should_not be_checked
+    else 
+      raise "unrecognized state '#{expected_state}'"
+    end
+  end
+
+  def verify_status_code_is(expected_status_code)
+    status_code.should == expected_status_code
+  end
+
+end
